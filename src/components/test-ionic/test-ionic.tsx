6c7bf4946d0633409a40160a9a06c01ec01db38d
lichun <zheyuan54@qq.com>
2023-03-11 17:44:39 +0800

    style: 测试使用 ::part

--- src/components/test-ionic/test-ionic.tsx
+++ src/components/test-ionic/test-ionic.tsx
@@ -1,8 +1,8 @@
 /*
- * @Description: 测试在 stencil 中使用 ionic（http://johnborg.es/2020/04/ionic-overlays-stencil.html）
+ * @Description: 测试在 stencil 中使用 ionic（https://ionicframework.com/docs/api/alert /////////////// http://johnborg.es/2020/04/ionic-overlays-stencil.html）
  * @Author: Lyrelion
  * @Date: 2023-03-11 17:01:42
- * @LastEditTime: 2023-03-11 17:12:53
+ * @LastEditTime: 2023-03-11 17:38:50
  * @FilePath: \stencil-first\src\components\test-ionic\test-ionic.tsx
  */
 import { Component, ComponentInterface, Host, h } from '@stencil/core';
@@ -41,9 +41,11 @@ export class TestIonic implements ComponentInterface {
   render() {
     return (
       <Host>
-        <ion-button onClick={(e: UIEvent) => this.openAlert(e)}>
+        {/* 指定 exportparts，这样用户就能修改 子组件 的 shadowDOM 样式了 */}
+        <ion-button exportparts="native" onClick={(e: UIEvent) => this.openAlert(e)}>
           <ion-icon slot="start" name="star"></ion-icon>
-          测试使用 ion-button
+          {/* 指定 part，这样用户就可以通过 ::part 修改 shadowDOM 样式了 */}
+          <span part="title" >测试使用 ion-button</span>
         </ion-button>
       </Host>
     );
